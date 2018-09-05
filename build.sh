17de19a107124abf440e1aa41632dcbc8120cd56
Nourelhouda1995 <nbenaffoune@gmail.com>
2018-09-05 10:52:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
       echo 'Checking Quality Gates'
-      mvn -B clean verify sonar:sonar \
-      -Dsonar.host.url=${SONAR_URL} \
-      -Dsonar.login=${SONAR_LOGIN} \
-      -Dsonar.projectKey=${SONAR_PROJECT} \
-      -Dsonar.organization=${SONAR_ORGANIZATION}
+    mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=Nourelhouda1995_Boomer -Dsonar.organization=nourelhouda1995-github -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=b5f7cdbab57d8a472de303c399b16ce8c888ea89 -Dsonar.pullrequest.github.repository=skokaina/devops -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
+
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
       echo 'Build and analyze pull request'
-      mvn -B clean verify sonar:sonar -Dsonar.analysis.mode=issues -Dsonar.host.url=https://sonarcloud.io/dashboard?id=Nourelhouda1995_Boomer -Dsonar.login=nourelhouda1995 -Dsonar.organization=nourelhouda1995-github -Dsonar.github.oauth=b5f7cdbab57d8a472de303c399b16ce8c888ea899 -Dsonar.github.repository=Nourelhouda1995/Boomer2 -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST};
+    mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=Nourelhouda1995_Boomer -Dsonar.organization=nourelhouda1995-github -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=b5f7cdbab57d8a472de303c399b16ce8c888ea89 -Dsonar.pullrequest.github.repository=skokaina/devops -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
+
 fi
