76756b8850bf5004ad809643a8bbbace7d3535f0
Nourelhouda1995 <nbenaffoune@gmail.com>
2018-09-05 09:01:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${T
       -Dsonar.login=${SONAR_LOGIN} \
       -Dsonar.projectKey=${SONAR_PROJECT} \
       -Dsonar.organization=${SONAR_ORGANIZATION}
+elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
+      echo 'Build and analyze pull request'
+      mvn -B clean verify sonar:sonar -Dsonar.analysis.mode=issues -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} -Dsonar.github.repository=Nourelhouda1995/Boomer2 -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST};
 fi
